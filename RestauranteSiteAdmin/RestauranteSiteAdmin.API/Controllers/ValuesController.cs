c9d6ac2391b1c65d69b8ec97ede8b6d2f7ed6867
Gabriel Moblicci <gabriel.moblicci@live.com>
2017-01-11 10:34:19 -0200

    Teste CI

--- RestauranteSiteAdmin/RestauranteSiteAdmin.API/Controllers/ValuesController.cs
+++ RestauranteSiteAdmin/RestauranteSiteAdmin.API/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace RestauranteSiteAdmin.API.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "Rodou!!!" };
         }
 
         // GET api/values/5
